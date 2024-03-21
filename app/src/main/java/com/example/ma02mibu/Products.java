a975dcb8e1e01392fd1170bfc94b108dcd0e56ea
IvanPartalo <ivanpartalo123@gmail.com>
2024-03-21 22:42:07 +0100

    switching to new activity

--- app/src/main/java/com/example/ma02mibu/Products.java
+++ app/src/main/java/com/example/ma02mibu/Products.java
@@ -1,8 +1,6 @@
 package com.example.ma02mibu;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Products extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_products);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Button productsButton = (Button) findViewById(R.id.productsButton);
-        productsButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Products.class);
-            startActivity(intent);
         });
     }
 }
