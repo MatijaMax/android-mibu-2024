3ca3989603dd1625b03f7a2e0acd494ad7935a28
IvanPartalo <ivanpartalo123@gmail.com>
2024-03-30 09:59:00 +0100

    creating product and navigating back

--- app/src/main/java/com/example/ma02mibu/FragmentTransition.java
+++ app/src/main/java/com/example/ma02mibu/FragmentTransition.java
@@ -1,19 +1,22 @@
 package com.example.ma02mibu;
 
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentTransition {
-    public static void to(Fragment newFragment, FragmentActivity activity, boolean addToBackstack, int layoutViewID)
+    public static void to(Fragment newFragment, FragmentActivity activity, boolean addToBackstack, int layoutViewID, String backStackTag)
     {
-        System.out.println("pogodjena metoda");
         FragmentTransaction transaction = activity
                 .getSupportFragmentManager()
                 .beginTransaction()
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .replace(layoutViewID, newFragment);
-        if(addToBackstack) transaction.addToBackStack(null);
+        if(addToBackstack)
+            transaction.addToBackStack(backStackTag);
 
         transaction.commit();
     }
--- app/src/main/java/com/example/ma02mibu/adapters/ProductListAdapter.java
+++ app/src/main/java/com/example/ma02mibu/adapters/ProductListAdapter.java
@@ -1,11 +1,13 @@
 package com.example.ma02mibu.adapters;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ma02mibu.R;
 import com.example.ma02mibu.model.Product;
@@ -21,8 +25,10 @@ import java.util.ArrayList;
 
 public class ProductListAdapter extends ArrayAdapter<Product> {
     private ArrayList<Product> aProducts;
+    Context context;
     public ProductListAdapter(Context context, ArrayList<Product> products){
         super(context, R.layout.product_card, products);
+        this.context = context;
         aProducts = products;
     }
     @Override
@@ -55,15 +61,48 @@ public class ProductListAdapter extends ArrayAdapter<Product> {
         TextView category = convertView.findViewById(R.id.product_category);
         TextView subCategory = convertView.findViewById(R.id.product_subcategory);
         TextView price = convertView.findViewById(R.id.product_price);
+        TextView oldPrice = convertView.findViewById(R.id.old_price);
+        ImageButton rightButton = convertView.findViewById(R.id.right_button);
+        ImageButton leftButton = convertView.findViewById(R.id.left_button);
+        handleRightButtonClick(rightButton, imageView, product);
+        handleLeftButtonClick(leftButton, imageView, product);
         if(product != null){
-            imageView.setImageResource(product.getImage());
+            int image = product.getImage().get(product.getCurrentImageIndex());
+            imageView.setImageResource(image);
             productName.setText(product.getName());
             productDescription.setText(product.getDescription());
             category.setText(product.getCategory());
             subCategory.setText(product.getSubCategory());
-            price.setText(product.getPrice());
+            oldPrice.setText(product.getPrice());
+            price.setText(product.getNewPrice());
+            oldPrice.setPaintFlags(oldPrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
         }
-
+        LinearLayoutManager layoutManager= new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        RecyclerView mRecyclerView = convertView.findViewById(R.id.event_type_tags);
+        mRecyclerView.setLayoutManager(layoutManager);
+        StringListAdapter listAdapter = new StringListAdapter(context, product.getEventTypes());
+        mRecyclerView.setAdapter(listAdapter);
         return convertView;
+    }
+    private void handleRightButtonClick(ImageButton rightButton, ImageView imageView, Product product){
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                product.setCurrentImageIndex(1);
+                int image = product.getImage().get(product.getCurrentImageIndex());
+                imageView.setImageResource(image);
+            }
+        });
+    }
+
+    private void handleLeftButtonClick(ImageButton leftButton, ImageView imageView, Product product){
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                product.setCurrentImageIndex(0);
+                int image = product.getImage().get(product.getCurrentImageIndex());
+                imageView.setImageResource(image);
+            }
+        });
     }
 }
