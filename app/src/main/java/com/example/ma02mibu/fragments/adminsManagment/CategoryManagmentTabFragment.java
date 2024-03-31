b01b0008c20ddbc779841c501c5a4f35ea4aeab4
ukinolo <ykithethird@gmail.com>
2024-03-31 22:14:46 +0200

    feat: added category managment list

--- app/src/main/java/com/example/ma02mibu/fragments/adminsManagment/CategoryManagmentTabFragment.java
+++ app/src/main/java/com/example/ma02mibu/fragments/adminsManagment/CategoryManagmentTabFragment.java
@@ -7,8 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.ma02mibu.R;
+import com.example.ma02mibu.adapters.adminsManagment.CategoryListAdapter;
+import com.example.ma02mibu.model.Category;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,17 +21,10 @@ import com.example.ma02mibu.R;
  * create an instance of this fragment.
  */
 public class CategoryManagmentTabFragment extends Fragment {
+    private CategoryListAdapter categoryListAdapter;
+    private ArrayList<Category> categories = new ArrayList<>();
+    public CategoryManagmentTabFragment() { }
 
-    public CategoryManagmentTabFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment CategoryManagmentTabFragment.
-     */
     public static CategoryManagmentTabFragment newInstance() {
         CategoryManagmentTabFragment fragment = new CategoryManagmentTabFragment();
         Bundle args = new Bundle();
@@ -37,12 +35,28 @@ public class CategoryManagmentTabFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createCategories();
+        categoryListAdapter = new CategoryListAdapter(getActivity(), categories);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_managment_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_category_managment_tab, container, false);
+
+        ListView categoriListView = view.findViewById(R.id.categoriesListView);
+        categoriListView.setAdapter(categoryListAdapter);
+
+        return view;
+    }
+    private void createCategories(){
+        categories.add(new Category(1L, "Ime kategorije 1", "Opis kategorije 1"));
+        categories.add(new Category(2L, "Ime kategorije 2", "Opis kategorije 2"));
+        categories.add(new Category(3L, "Ime kategorije 3", "Opis kategorije 3"));
+        categories.add(new Category(4L, "Ime kategorije 4", "Opis kategorije 4"));
+        categories.add(new Category(5L, "Ime kategorije 5", "Opis kategorije 5"));
+        categories.add(new Category(6L, "Ime kategorije 6", "Opis kategorije 6"));
+        categories.add(new Category(7L, "Ime kategorije 7", "Opis kategorije 7"));
     }
 }
